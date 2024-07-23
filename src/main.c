bfd33ab27defd6ec3e2c5bc699255187d9f8bf87
vinceh121 <contact@vinceh121.me>
2024-07-23 02:26:07 +0200

    feat: get the nomads window, and repeatly lock the cursor inside it

--- src/main.c
+++ src/main.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <windef.h>
+#include <basetsd.h>
+#include <winbase.h>
+#include <winuser.h>
+
+const LPCWSTR class = L"nD3D8Server window class";
+const LPCWSTR name = L" Nomads";
+
 int main(void) {
-	printf("Hello world!\n");
+	HWND nomads = FindWindowW(class, name);
+
+	if (!nomads) {
+		printf("Couldn't find nomads :(\n");
+		return -1;
+	}
+
+	WINDOWINFO wininfo;
+	wininfo.cbSize = sizeof(WINDOWINFO);
+
+	if (!GetWindowInfo(nomads, &wininfo)) {
+		printf("Couldn't get the window's infos\n");
+		return -2;
+	}
+
+	RECT newClip = wininfo.rcClient;
+
+	printf("Nomads window: %d, %d, %d, %d\n", newClip.left, newClip.top, newClip.right, newClip.bottom);
+
+	RECT oldClip;
+	GetClipCursor(&oldClip);
+
+	while ((nomads = FindWindowW(class, name))) {
+		ClipCursor(&newClip);
+		Sleep(500);
+	}
+
+	printf("Nomads is gone! Shutting down\n");
+
+	ClipCursor(&oldClip);
+
 	return EXIT_SUCCESS;
 }
 
