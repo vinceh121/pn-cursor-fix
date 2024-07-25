d6157eb1bd060f12309dbaf7187bc614b2e889d0
vinceh121 <contact@vinceh121.me>
2024-07-25 04:10:13 +0200

    feat: wait for nomads to show up for 10s (clses #2)

--- src/main.c
+++ src/main.c
@@ -10,11 +10,22 @@ const LPCWSTR class = L"nD3D8Server window class";
 const LPCWSTR name = L" Nomads";
 
 int main(void) {
+	// unbuffer stdout
+	setvbuf(stdout, NULL, _IONBF, 0);
+
 	HWND nomads = FindWindowW(class, name);
+
+	for (int i = 10; i > 0 && !nomads; i--) {
+		nomads = FindWindowW(class, name);
+		printf("Couldn't find nomads :(\nTrying again %d more times\n", i);
+		Sleep(1000);
+	}
 
 	if (!nomads) {
 		printf("Couldn't find nomads :(\n");
 		return -1;
+	} else {
+		printf("Found nomads :D\n");
 	}
 
 	RECT oldClip;
